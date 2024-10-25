44e922275658b35e81e13939ef54b94f379566a0
bhugo <41992011+bhugoVilela@users.noreply.github.com>
2024-10-25 15:34:01 +0100

    try to fix workflow

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-nvim --headless -c "PlenaryBustedDirectory tests/palette {minimal_init = './tests/minimal_init.lua'}"
+# nvim --headless -c "PlenaryBustedDirectory tests/palette {minimal_init = './tests/minimal_init.lua'}"
+nvim --headless --clean -u ./tests/minimal_init.lua -c "PlenaryBustedDirectory tests/palette/ {minimal_init = './tests/minimal_init.lua', sequential = true}"
 # nvim -c "PlenaryBustedDirectory tests/palette {minimal_init = './tests/minimal_init.lua'}"
 
 
